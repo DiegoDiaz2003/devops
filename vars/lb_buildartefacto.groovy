d46050eddd747496fabe9d25160a610221ff1197
DiegoDiaz2003 <diegoalejandrodiazramos@gmail.com>
2025-01-14 09:37:38 -0500

    Correcion final?

--- vars/lb_buildartefacto.groovy
+++ vars/lb_buildartefacto.groovy
@@ -2,7 +2,7 @@ package org.devops
 
 def clone() {
     echo "Ejecutando clone() desde lb_buildartefacto"
-    git branch: "${env.nameBranch}", url: "${env.UrlGitHub}"
+    git url: 'https://github.com/DiegoDiaz2003/devops.git', branch: 'develop', credentialsId: 'GitHub-Diego'
 }
 
 def install() {
