6c65e17e1c7f535e447562ef82c95dc026689366
DiegoDiaz2003 <diegoalejandrodiazramos@gmail.com>
2025-01-14 09:28:27 -0500

    Correcion final?

--- src/org/devops/lb_buildartefacto.groovy
+++ src/org/devops/lb_buildartefacto.groovy
@@ -1,9 +1,11 @@
 package org.devops
 
 def clone() {
+    echo "Ejecutando clone() desde lb_buildartefacto"
     git branch: "${env.nameBranch}", url: "${env.UrlGitHub}"
 }
 
 def install() {
+    echo "Ejecutando install() desde lb_buildartefacto"
     sh 'npm install'
 }
