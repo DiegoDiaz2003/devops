372c62ba1ba8f0f154b6ed7fa455a2c81a98a536
DiegoDiaz2003 <diegoalejandrodiazramos@gmail.com>
2025-01-14 09:50:40 -0500

    Correcion final?

--- vars/lb_analisissonarqube.groovy
+++ vars/lb_analisissonarqube.groovy
@@ -7,8 +7,8 @@ def testCoverage(){
 def analisisSonar(gitName){
     def scannerHome = tool 'sonar-scaner'
     if (scannerHome) {
-        withSonarQubeEnv ( 'sonar-scanner ' ){
-            sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${gitName} -Dsonar.projectName=${gitName} -Dsonar.sources=${env.source} -Dsonar.tests=src/_test__ -Dsonar.exclusions='**/*.test.js' -Dsonar.testExecutionReportPaths=./test-report.xml -Dsonar.javascript.lcov.reportPaths=./coverage/lcov.info"
+        withSonarQubeEnv ( 'sonar-scaner ' ){
+            sh "${scannerHome}/bin/sonar-scaner -Dsonar.projectKey=${gitName} -Dsonar.projectName=${gitName} -Dsonar.sources=${env.source} -Dsonar.tests=src/_test__ -Dsonar.exclusions='**/*.test.js' -Dsonar.testExecutionReportPaths=./test-report.xml -Dsonar.javascript.lcov.reportPaths=./coverage/lcov.info"
         }
     } else{
         error 'SonarQube Scanner not found'
