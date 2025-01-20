ffa5a5589441883d434233ecf1ac61a71c96029f
DiegoDiaz2003 <diegoalejandrodiazramos@gmail.com>
2025-01-20 17:56:28 -0500

    OSWAP FInal 2

--- vars/pipelineGeneral.groovy
+++ vars/pipelineGeneral.groovy
@@ -37,13 +37,18 @@ def call(Map args) {
                 }
             }
            stage('OWASP Scan') {
-                steps {
-                    script {
-                             load 'lb_owasp.groovy'
-                             AnalisisOwasp("devops")
-                      }
-                 }
+            steps {
+                script {
+                    if (fileExists('lb_owasp.groovy')) {
+                      load 'lb_owasp.groovy'
+                     AnalisisOwasp("devops")
+            } else {
+                error "Archivo lb_owasp.groovy no encontrado"
+                    }
+                }
             }
+        }
+
 
         }
     }
