6b76245e33ad3d6bab275a3607835323d12b6959
DiegoDiaz2003 <diegoalejandrodiazramos@gmail.com>
2025-01-20 16:37:27 -0500

    ejecucion

--- vars/lb_buildimagen.groovy
+++ vars/lb_buildimagen.groovy
@@ -3,6 +3,7 @@ package org.devops
 def call(String projectGitName) {
     echo "Construyendo la imagen Docker para: diegodiaz12/${projectGitName}"
     try {
+        // Construir la imagen Docker
         sh "docker build -t diegodiaz12/${projectGitName} ."
     } catch (Exception e) {
         error "Error al construir la imagen Docker: ${e.message}"
--- vars/lb_publicardockerhub.groovy
+++ vars/lb_publicardockerhub.groovy
@@ -1,8 +1,13 @@
 def call(String projectName, String dockerHubUsername) {
     withCredentials([usernamePassword(credentialsId: "${env.TOKEN_ID}", 
     passwordVariable: 'DOCKERHUB_PASSWORD', usernameVariable: 'DOCKERHUB_USERNAME')]) {
-        sh "docker login -u ${env.DOCKERHUB_USERNAME} -p ${env.DOCKERHUB_PASSWORD}"
-        sh "docker tag ${projectName} ${env.DOCKERHUB_USERNAME}/${projectName}"
-        sh "docker push ${env.DOCKERHUB_USERNAME}/${projectName}"
+        // Iniciar sesión en Docker Hub de manera segura
+        sh "echo \$DOCKERHUB_PASSWORD | docker login -u ${dockerHubUsername} --password-stdin"
+        
+        // Etiquetar la imagen
+        sh "docker tag ${projectName} ${dockerHubUsername}/${projectName}"
+        
+        // Publicar la imagen en Docker Hub
+        sh "docker push ${dockerHubUsername}/${projectName}"
     }
 }
