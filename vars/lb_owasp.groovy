6ef7496be4945ab3de754d008128fe500f226338
DiegoDiaz2003 <diegoalejandrodiazramos@gmail.com>
2025-01-20 17:21:37 -0500

    OSWAP FInal

--- vars/lb_owasp.groovy
+++ vars/lb_owasp.groovy
@@ -3,7 +3,7 @@ def call(String url) {
     docker run --rm -v ProjectOwasp:/zap/wrk/:rw \
     --user root --network=jenkinsonar_network_containers \
     -t edansama96/zap2docker-stable:latest \
-    zap-full-scan.py -t http://localhost:5174 -r Projectowasp.html -I
+    zap-full-scan.py -t http://172.22.0.2:5174 -r Projectowasp.html -I
     """
 
 }
