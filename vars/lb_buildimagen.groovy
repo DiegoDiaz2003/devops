9cb28ae3a252bf3043367fb9999c8d88b0a54bd6
DiegoDiaz2003 <diegoalejandrodiazramos@gmail.com>
2025-01-20 16:31:50 -0500

    Docker

--- vars/lb_buildimagen.groovy
+++ vars/lb_buildimagen.groovy
@@ -1,5 +1,10 @@
 package org.devops
 
 def call(String projectGitName) {
-    sh "docker build -t diegodiaz12/${projectGitName} ."
+    echo "Construyendo la imagen Docker para: diegodiaz12/${projectGitName}"
+    try {
+        sh "docker build -t diegodiaz12/${projectGitName} ."
+    } catch (Exception e) {
+        error "Error al construir la imagen Docker: ${e.message}"
+    }
 }
